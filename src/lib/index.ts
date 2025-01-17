81cf8a9db1a23562a781d34dcc4bc137361570bc
inflection-prashant <prashant.kharade@inflectionzone.com>
2025-01-17 16:18:45 +0530

    "All drag and drop code with documentation"

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,26 @@
 // Reexport your entry components here
+export { draggable } from './utils/drag-and-drop';
+export { dropzone } from './utils/drag-and-drop';
+
+export interface Card {
+    id: number;
+    content: string;
+}
+
+export enum ViewType {
+    LIST = 'list',
+    GRID = 'grid'
+}
+
+// Types
+export type Button = {
+    id: number;
+    name: string;
+    icon: string;
+};
+
+export type DroppedItem = Button & {
+    dropTime: string;
+    uniqueId: string;
+};
+
