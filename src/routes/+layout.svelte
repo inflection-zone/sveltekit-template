92c070a71bd37f84b0977c508d73c96344d16548
mayur-bankar-inflection <mayur.s.bankar@inflectionzone.com>
2025-02-17 16:29:20 +0530

    conditional Layouts added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,15 @@
 	import { addToast } from '$lib/components/toast/toast.store';
 	import Toasts from '$lib/components/toast/toasts.svelte';
 	import { initFlash } from 'sveltekit-flash-message/client';
-
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	import {
+		getPublicFooterLink,
+		getPublicFooterText,
+		getPublicLogoImageSource
+	} from '$lib/components/themes/theme.selector';
+//////////////////////////////////////////////////////////////////////////////////////////////////
+	const logoImageSource = getPublicLogoImageSource();
+	const footerText = `© ${new Date().getFullYear()} ${getPublicFooterText()}`;
+	const footerLink = getPublicFooterLink();
 
 	const flash = initFlash(page);
 
@@ -19,7 +26,19 @@
 		});
 		flash.set(undefined);
 	});
+
+	// Derived store to check if the current route starts with "/users"
+	$: hideLogoAndFooter = $page.url.pathname.startsWith('/users');
 </script>
 
 <Toasts />
 <slot />
+
+{#if !hideLogoAndFooter}
+	<div class="absolute top-4 left-4 flex items-center">
+		<img src={logoImageSource} alt="Logo" class="px-4 w-32 h-auto" />
+	</div>
+	<footer class="fixed bottom-0 w-full text-center py-4">
+		<a href={footerLink} class="!text-black">{footerText}</a>
+	</footer>
+{/if}
